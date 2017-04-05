0460cc6ed6dec382220f3e997a64c9123bba6b6d
Khoa Truong <Khoa Truong>
2017-04-05 00:42:35 -0500

    Everything except the assets

---
+++ track_it/js/proto1.js
@@ -0,0 +1,59 @@
+main = [];
+function track() {
+
+  var messy_list = $('form').serializeArray();
+
+  messy_list.forEach( function(e) {
+    if (e.value != "") {
+      main.push(e.value);
+    }
+  });
+
+  if (JSON.stringify(main) != "[]"){
+    sessionStorage.setItem("db", JSON.stringify(main));
+    location.href = "./currActivity.html";
+  }
+  else {
+    $(".alert").show();
+  }
+
+}
+document.onkeypress = (function (e) {
+  if (e.which == 13){
+    e.preventDefault();
+    track();
+  }
+});
+
+// document.onclick = (function (e) {
+//
+//   if (document.getElementsByClassName("alert")){
+//     $(".alert").hide();
+//   }
+// });
+
+//Set storage value to element;
+function setStorage(id){
+
+  if(document.getElementById(id)){
+
+    var setEl = document.getElementById(id);
+
+    if (sessionStorage.getItem("db")) {
+      var saved = JSON.parse(sessionStorage.getItem("db"));
+      setEl.innerHTML = saved;
+    }
+  }
+}
+
+setStorage("currAct");
+setStorage("timeline");
+
+//Set Date on TimeLine
+function gDate() {
+  var d = new Date();
+  var dateEl = document.getElementById('date');
+  // 'Setting' data-attributes using setAttribute
+  dateEl.setAttribute('data-date',d.toDateString());
+  return;
+}
